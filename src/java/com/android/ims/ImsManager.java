244d095011bfbdc98348b83aff39ab081f0ebf3f
Suchand Ghosh <suchan@codeaurora.org>
2017-04-12 12:35:25 +0530

    IMS: Conference URI support.
    Add code to call correct call.start()
    based on the conference URI.
    Setting the conference host property of the ImsCall,
    on which a group conference dial request is sent, to
    true.
    This correctly allows the group conference call to
    be identified as a conference call hosted on the UE.
    
    CRs-Fixed: 1023212
    Change-Id: Iea98afc0658b8526671c305e8f2f61f3882d2b1c

--- src/java/com/android/ims/ImsManager.java
+++ src/java/com/android/ims/ImsManager.java
@@ -49,6 +49,7 @@ import com.android.ims.internal.IImsUt;
 import com.android.ims.internal.ImsCallSession;
 import com.android.ims.internal.IImsConfig;
 import com.android.internal.annotations.VisibleForTesting;
+import com.android.internal.telephony.TelephonyProperties;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -1716,8 +1717,9 @@ public class ImsManager {
 
         call.setListener(listener);
         ImsCallSession session = createCallSession(sessionId, profile);
-
-        if ((callees != null) && (callees.length == 1)) {
+        boolean isConferenceUri = profile.getCallExtraBoolean(
+                TelephonyProperties.EXTRAS_IS_CONFERENCE_URI, false);
+        if (!isConferenceUri && (callees != null) && (callees.length == 1)) {
             call.start(session, callees[0]);
         } else {
             call.start(session, callees);
